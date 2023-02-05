2a85db52c5ded95a10ddd24a16de54ed77bd497f
majid <majid@bictory.io>
2023-02-05 14:34:56 +0330

    better config for autocomplete in search mode

--- init.lua
+++ init.lua
@@ -34,8 +34,8 @@ vim.opt.syntax= 'ON'
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
 
-vim.cmd('highlight LineNr ctermfg=gray')
 vim.cmd('filetype plugin indent on')
+vim.cmd('highlight LineNr ctermfg=gray')
 vim.cmd('au BufRead * normal zR')
 
 -- vsnip configs -------------------------------------
@@ -132,10 +132,10 @@ nmap('ga',':lua vim.lsp.buf.code_action()<CR>')
 
 
 require('mason').setup()
-local rt = {
+rt = require('rust-tools')
+rt.setup({
 	tools = { -- rust-tools options
         autoSetHints = true,
-	hover_with_actions = true,
         inlay_hints = {
             show_parameter_hints = false,
             parameter_hints_prefix = "",
@@ -143,14 +143,19 @@ local rt = {
         },
     },
     server = {
+	     on_attach = function(_, bufnr)
+      		-- Hover actions
+      		vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      		-- Code action groups
+      		vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    	     end,
             ["rust-analyzer"] = {
                 checkOnSave = {
                     command = "clippy"
                 }, 
             },
     },
-}
-require('rust-tools').setup(rt)
+}) 
 require("mason-lspconfig").setup({
     ensure_installed = { "gopls" }
 })
@@ -210,18 +215,22 @@ autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 -- Completion Plugin Setup
 local cmp = require'cmp'
 
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
 })
-cmp.setup.cmdline('?', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
 })
 
 cmp.setup({
@@ -252,8 +261,8 @@ cmp.setup({
     { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
     { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
     { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'vsnip' },         		    -- nvim-cmp source for vim-vsnip 
     { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
     { name = 'calc'},                               -- source for math calculation
   },
   window = {
@@ -300,21 +309,78 @@ require('nvim-web-devicons').get_icons()
 vim.cmd('colorscheme molokai') 
 vim.g.rehash256 = true
 vim.g.molokai_original = true
+
+
+require("todo-comments").setup({
+keywords = {
+    --FIX = {
+      --icon = " ", -- icon used for the sign, and in search results
+      --color = "error", -- can be a hex color, or a named color (see below)
+      --alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+      ---- signs = false, -- configure signs for some keywords individually
+    --},
+    TODO = { icon = " ", color = "info" },
+    HACK = { icon = " ", color = "warning" },
+    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    --PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    --NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+    --TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+  },
+  gui_style = {
+    fg = "NONE", -- The gui style to use for the fg highlight group.
+    bg = "BOLD", -- The gui style to use for the bg highlight group.
+  },
+  highlight = {
+    multiline = true, -- enable multine todo comments
+    multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+    multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+    before = "", -- "fg" or "bg" or empty
+    keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+    after = "fg", -- "fg" or "bg" or empty
+    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+    comments_only = true, -- uses treesitter to match keywords in comments only
+    max_line_len = 400, -- ignore lines longer than this
+    exclude = {}, -- list of file types to exclude highlighting
+  },
+  colors = {
+    error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+    warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+    info = { "DiagnosticInfo", "#2563EB" },
+    hint = { "DiagnosticHint", "#10B981" },
+    default = { "Identifier", "#7C3AED" },
+    test = { "Identifier", "#FF00FF" }
+  },
+  search = {
+    command = "rg",
+    args = {
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+    },
+    -- regex that will be used to match keywords.
+    -- don't replace the (KEYWORDS) placeholder
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+  },
+})
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'williamboman/mason.nvim'    
   use 'williamboman/mason-lspconfig.nvim'
   use 'neovim/nvim-lspconfig'
+  use { 'fatih/vim-go', ft = 'go' } 
   use 'simrat39/rust-tools.nvim'
-  use { 'fatih/vim-go', run = ':UpdateRemotePlugins', ft = 'go' } 
-  use 'fatih/molokai' 
+  use {'fatih/molokai', ft = 'go'} 
   use 'junegunn/fzf'
   use 'junegunn/fzf.vim' 
   use 'jiangmiao/auto-pairs' 
   use 'AndrewRadev/splitjoin.vim' 
   use 'preservim/nerdcommenter' 
   use 'nvim-treesitter/nvim-treesitter'
-  use 'rust-lang/rust.vim' 
+  use {'rust-lang/rust.vim', ft = 'rust'}
   use 'kyazdani42/nvim-web-devicons' 
   use 'kyazdani42/nvim-tree.lua'
 -- Completion framework:
@@ -329,5 +395,6 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-buffer'                            
   use 'hrsh7th/vim-vsnip'
   use "rafamadriz/friendly-snippets"
+  use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim"}
 end)
 
